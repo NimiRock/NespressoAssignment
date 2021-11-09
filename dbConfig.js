1dcf87dd19fe0a2687c69c1e5ba1525d3d51e956
Nimrod Rokach <nimrod.rock@gmail.com>
2021-11-10 01:12:29 +0200

    set db to createpool

--- dbConfig.js
+++ dbConfig.js
@@ -7,16 +7,11 @@ const mysql = require("mysql");
 // 	database: "nespressocustomers",
 // });
 
-const connection = mysql.createConnection({
+const connection = mysql.createPool({
 	host: "us-cdbr-east-04.cleardb.com",
 	user: "b9f04cf6d85a61",
 	password: "c87945a0",
 	database: "heroku_1c85dce2e7470a5",
-});
-
-connection.connect((err) => {
-	if (err) throw err;
-	console.log("connected to mySql");
 });
 
 const Query = (q, ...values) => {
